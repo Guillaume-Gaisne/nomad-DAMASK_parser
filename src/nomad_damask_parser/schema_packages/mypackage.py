67d5acd7c4191fc5df17060040d6858e8901c3d7
Guillaume Gaisne <guillaume.gaisne@esrf.fr>
2024-06-05 09:25:48 +0200

    Create the DAMASK schema embryo

--- src/nomad_damask_parser/schema_packages/mypackage.py
+++ src/nomad_damask_parser/schema_packages/mypackage.py
@@ -10,10 +10,11 @@ if TYPE_CHECKING:
         BoundLogger,
     )
 
+from numpy import array
 from nomad.config import config
 from nomad.datamodel.data import Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
-from nomad.metainfo import Quantity, SchemaPackage
+from nomad.metainfo import Quantity, SchemaPackage, MSection, SubSection, MEnum
 
 configuration = config.get_plugin_entry_point(
     'nomad_damask_parser.schema_packages:mypackage'
@@ -22,17 +23,68 @@ configuration = config.get_plugin_entry_point(
 m_package = SchemaPackage()
 
 
+class Dataset(MSection):
+    name = Quantity(type=str, description="Name of the dataset")
+    unit = Quantity(type=str, description="Unit of the data in this dataset")
+    shape = Quantity(type=array, description="Shape of the data array")
+    description = Quantity(type=str, description="Information about the nature of the dataset")
+    data = Quantity(type=str, description="Placeholder for now for the data")
+
+###############################################################################
+class PhaseDataset(MSection):
+    phase_name = Quantity(type=str, description="User defined name of the phase")
+    phase_field = Quantity(type=MEnum('mechanical', 'damage', 'thermal'))
+    phase_datasets = SubSection(section=Dataset, repeats=True)
+
+class HomogenizationDataset(MSection):
+    homogenization_name = Quantity(type=str, description="User defined name of the homogenization")
+    homogenization_field = Quantity(type=MEnum('mechanical', 'damage', 'thermal'))
+    homogenization_datasets = SubSection(section=Dataset, repeats=True)
+
+class GeometryDataset(MSection):
+    geometry_datasets = SubSection(section=Dataset, repeats=True)
+
+class Increment(MSection):
+    increment_name = Quantity(type=str, description="Name of the increment")
+    increment_geometry = SubSection(section=GeometryDataset, repeats=False)
+    increment_homogenization = SubSection(section=HomogenizationDataset, repeats=False)
+    increment_phase = SubSection(section=PhaseDataset, repeats=False)
+
+###############################################################################
+class SetupFile(MSection):
+    name = Quantity(type=str, description="Name of the setup file")
+
+class Setup(MSection):
+    description = Quantity(type=str, description="Information about the setup section")
+    setup_filenames = SubSection(section=SetupFile, repeats=True)
+
+###############################################################################
+class Geometry(MSection):
+    cells = Quantity(type=float, shape=[3], description="Values corresponding to the cells")
+    origin = Quantity(type=float, shape=[3], description="Values corresponding to the origin")
+    size = Quantity(type=float, shape=[3], description="Values corresponding to the size")
+
+###############################################################################
+class CellTo(MSection):
+    description = Quantity(type=str, description="Information about the cell_to section")
+    cell_to_datasets = SubSection(section=Dataset, repeats=True)
+
+###############################################################################
+###############################################################################
 class MySchema(Schema):
-    name = Quantity(
-        type=str, a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
-    )
-    message = Quantity(type=str)
+    code_version = Quantity(type=str, description="Version of DAMASK used to produce the results of this simulation")
+    number_increments = Quantity(type=int, shape=[], description="Number of increments in the simulation")
+    phase_names = Quantity(type=str, shape=['*'], description="Unique names of the different phases used in the simulation")
+    homoginezation_names = Quantity(type=str, shape=['*'], description="Unique names of the different homogenizations used in the simulation")
+    points_number = Quantity(type=int, shape=[], description="Number of points in the simulation")
+    solver_name = Quantity(type=str, description="Name of the solver used for the simulation")
+    cell_to = SubSection(section=CellTo, repeats=False)
+    geometry = SubSection(section=Geometry, repeats=False)
+    increments = SubSection(section=Increment, repeats=True)
+    setup = SubSection(sectin=Setup, repeats=False)
 
-    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
-        super().normalize(archive, logger)
-
-        logger.info('MySchema.normalize', parameter=configuration.parameter)
-        self.message = f'Hello {self.name}!'
+    # def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+    #     super().normalize(archive, logger)
 
 
 m_package.__init_metainfo__()
